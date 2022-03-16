3891e7b8ae58dd59f7075b202faec7f69dc8bcb4
Pablo Villalba <garanaw@gmail.com>
2022-03-16 23:24:22 +0000

    Adds more configuration

--- app/Application/Network/routes.php
+++ app/Application/Network/routes.php
@@ -6,6 +6,6 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 $router = app(Router::class);
 
-$router->get('/networks', Index::class)
+$router->get('/', Index::class)
     ->name('networks.index')
     ->middleware('web');
