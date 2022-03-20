f6d087a4701915d24c83aed20b428930de8bcf03
Pablo Villalba <garanaw@gmail.com>
2022-03-20 14:27:11 +0000

    Adds domain service providers

---
+++ app/Application/Network/Middleware/LoadFrom.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Network\Middleware;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class LoadFrom
+{
+    public function __construct(private Application $app)
+    {
+    }
+
+    public function handle(Request $request, \Closure $next)
+    {
+        $this->getProviders()->each(
+            fn (string $provider) => $this->app->register($provider)
+        );
+
+        return $next($request);
+    }
+
+    private function getProviders(): Collection
+    {
+        return collect($this->app['config']->get('domain.network.providers', []));
+    }
+}
---
+++ config/domain.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'network' => [
+        'service_providers' => [
+            App\Domain\Network\Providers\ViewServiceProvider::class,
+        ],
+    ],
+];
