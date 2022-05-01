092d7d21611c4cac19badf364080530b8c4826cf
alun <garanaw@gmail.com>
2022-05-01 11:57:30 +0100

    Imports class from namespace

--- app/Application/HttpKernel.php
+++ app/Application/HttpKernel.php
@@ -3,6 +3,7 @@
 namespace Application;
 
 use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
 class HttpKernel extends Kernel
 {
@@ -36,13 +37,13 @@ class HttpKernel extends Kernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SubstituteBindings::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            SubstituteBindings::class,
         ],
     ];
 
